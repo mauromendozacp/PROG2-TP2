1d75061216b34606d6e875bcaf6a001db8bc16c3
jeesssik <jeesssik@gmail.com>
2024-10-25 16:13:34 -0300

    inicio de branch y commit en la linea de error inicial

--- Assets/Game/System/Loading/Scripts/LoadingManager.cs
+++ Assets/Game/System/Loading/Scripts/LoadingManager.cs
@@ -14,7 +14,7 @@ public class LoadingManager : MonoBehaviour
     private static readonly Dictionary<SceneGame, string> sceneNames = new Dictionary<SceneGame, string>()
     {
         { SceneGame.Menu, "Menu" },
-       // { SceneGame.Shooter, "Shooter" },
+       // { SceneGame.Shooter, "Shooter" },  // ojo con esto que daba error
         { SceneGame.Loading, "Loading" }
     };
 

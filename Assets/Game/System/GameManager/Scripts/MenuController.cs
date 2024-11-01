4777a3287bfe4fd98e5c16e4ccfce4e91479f9bb
jeesssik <jeesssik@gmail.com>
2024-11-01 09:05:13 -0300

    Add credits canvas functionality to menu controller

--- Assets/Game/System/GameManager/Scripts/MenuController.cs
+++ Assets/Game/System/GameManager/Scripts/MenuController.cs
@@ -7,12 +7,13 @@ public class MenuController : MonoBehaviour
 {
     private GameManager gameManager;
     [SerializeField] private GameObject menuCanvas; 
+    [SerializeField] private Canvas creditsCanvas;
 
     private void Start()
     {
         // Get the GameManager instance at startup
         gameManager = GameManager.Instance;
-        //Debug.Log(gameManager); // This should display the GameManager instance in the console
+        creditsCanvas.gameObject.SetActive(false);
     }
 
     // Method to handle the start game button
@@ -23,8 +24,21 @@ public class MenuController : MonoBehaviour
 
     GameManager.Instance.LoadingManager.TransitionScene(SceneGame.Gameplay, () =>
     {
-        // Código a ejecutar cuando la transición de escena se complete
+        // Code to execute when the scene transition is complete
     });
+    }
+        // Method to handle the credits canva
+      public void ShowCredits()
+    {
+        menuCanvas.gameObject.SetActive(false);
+        creditsCanvas.gameObject.SetActive(true);
+    }
+
+    // Method to handle the back button in the credits canvas
+    public void ShowMenu()
+    {
+        creditsCanvas.gameObject.SetActive(false);
+        menuCanvas.gameObject.SetActive(true);
     }
 
     // Other methods to handle other buttons 
